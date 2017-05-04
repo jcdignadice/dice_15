db022ae3560fbb6acacb9d42ea2c9a588e5d067f
Dice <cdignadice.bos@gmail.com>
2017-05-04 15:33:34 +0800

    Initial commit

---
+++ scripts/data.js
@@ -0,0 +1,107 @@
+var profile = {
+  info: {
+    first_name: "John Christian",
+    middle_name: "Cosico",
+    last_name: "Dignadice",
+    title: "Junior Web Designer",
+    age: "20",
+    location: "San Pablo City, Laguna",
+    mobile: "+639468154549",
+    email: "johnchristiandignadice@gmail.com",
+    course: "Bachelor of Science in Computer Science",
+    height: "5'8",
+    weight: "60kg",
+    religion: "Roman Catholic"
+  },
+  // social: {
+  //   facebook: "https://www.facebook.com/jigzimon",
+  //   linkedin: "https://www.linkedin.com/in/jigno-alfred-venezuela-757600b3/",
+  //   github: "https://github.com/iamjigz",
+  //   codepen: "http://codepen.io/iamjigz/",
+  //   stack_overflow: "http://stackoverflow.com/users/4918036/jigno-alfred-venezuela"
+  // }
+};
+
+var school = {
+  college: {
+    school: "Laguna State Polytechnic University",
+    location: "San Pablo City, Laguna",
+    period: '2013 - 2017'
+  },
+  hs: {
+    school: "Laguna College",
+    location: "San Pablo City, Laguna",
+    period: '2009 - 2013'
+  },
+  elem: {
+    school: "San Pablo Central School",
+    location: "San Pablo City, Laguna",
+    period: '2003 - 2009'
+  }
+};
+
+var skills = [{
+  name: "Web Crawling",
+  url: 'https://en.wikipedia.org/wiki/Web_crawler'
+}, {
+  name: "Git",
+  url: 'https://git-scm.com/'
+}, {
+  name: "Data Extraction",
+  url: 'https://en.wikipedia.org/wiki/Data_extraction'
+}, {
+  name: "Pug.js",
+  url: 'https://pugjs.org/api/getting-started.html'
+}, {
+  name: "Express.js",
+  url: 'https://expressjs.com/'
+}, {
+  name: "Node.js",
+  url: 'https://expressjs.com/'
+}, {
+  name: "Angular.js",
+  url: 'https://angularjs.org/'
+}, {
+  name: "Handlebars.js",
+  url: 'http://handlebarsjs.com/'
+}, {
+  name: "SASS",
+  url: 'http://sass-lang.com/'
+}, {
+  name: "Stylus",
+  url: 'http://stylus-lang.com/'
+}, {
+  name: "MongoDB",
+  url: 'https://www.mongodb.com/'
+}, {
+  name: "Microsoft SQL Server",
+  url: 'https://www.microsoft.com/en-us/sql-server'
+}, {
+  name: "Google Apps Script",
+  url: 'https://developers.google.com/apps-script/'
+}, {
+  name: "API",
+  url: 'https://en.wikipedia.org/wiki/Application_programming_interface'
+}];
+
+var languages = [{
+  name: "PHP",
+  percent: "60%",
+  offset: '351'
+}, {
+  name: "VB.NET",
+  percent: "75%",
+  offset: '481'
+}, {
+  name: "Python",
+  percent: "45%",
+  offset: '281'
+}, {
+  name: "Javascript",
+  percent: "80%",
+  offset: '521'
+}, {
+  name: "HTML/CSS",
+  percent: "58%",
+  offset: '321'
+}];
